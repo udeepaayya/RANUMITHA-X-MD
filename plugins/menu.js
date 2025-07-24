3f00ad37678176979d6c64297a54cb3155728a1f
Ranumithaofc <hirukaranumithagitbota@gmail.com>
2025-07-24 17:55:02 +0530

    Add files via upload

---
+++ plugins/menu.js
@@ -0,0 +1,106 @@
+const { readEnv } = require("../lib/database");
+const { cmd, commands } = require("../command");
+
+cmd(
+  {
+    pattern: "menu",
+    alise: ["getmenu"],
+    desc: "get cmd list",
+    category: "main",
+    filename: __filename,
+  },
+  async (
+    robin,
+    mek,
+    m,
+    {
+      from,
+      quoted,
+      body,
+      isCmd,
+      command,
+      args,
+      q,
+      isGroup,
+      sender,
+      senderNumber,
+      botNumber2,
+      botNumber,
+      pushname,
+      isMe,
+      isOwner,
+      groupMetadata,
+      groupName,
+      participants,
+      groupAdmins,
+      isBotAdmins,
+      isAdmins,
+      reply,
+    }
+  ) => {
+    try {
+      const config = await readEnv();
+      let menu = {
+        main: "",
+        download: "",
+        group: "",
+        owner: "",
+        convert: "",
+        search: "",
+      };
+
+      for (let i = 0; i < commands.length; i++) {
+        if (commands[i].pattern && !commands[i].dontAddCommandList) {
+          menu[
+            commands[i].category
+          ] += `${config.PREFIX}${commands[i].pattern}\n`;
+        }
+      }
+
+      let madeMenu = `👋 *Hello  ${pushname}*
+
+
+| *MAIN COMMANDS* |
+    ▫️.alive
+    ▫️.menu
+    ▫️.ai <text>
+    ▫️.system
+    ▫️.owner
+| *DOWNLOAD COMMANDS* |
+    ▫️.song <text>
+    ▫️.video <text>
+    ▫️.fb <link>
+| *GROUP COMMANDS* |
+${menu.group}
+| *OWNER COMMANDS* |
+    ▫️.restart
+    ▫️.update
+| *CONVERT COMMANDS* |
+    ▫️.sticker <reply img>
+    ▫️.img <reply sticker>
+    ▫️.tr <lang><text>
+    ▫️.tts <text>
+| *SEARCH COMMANDS* |
+${menu.search}
+
+
+🥶𝐌𝐚𝐝𝐞 𝐛𝐲 𝐒_𝐈_𝐇_𝐈_𝐋_𝐄_𝐋🥶
+
+> ROBIN MENU MSG
+`;
+      await robin.sendMessage(
+        from,
+        {
+          image: {
+            url: "https://raw.githubusercontent.com/Dark-Robin/Bot-Helper/refs/heads/main/autoimage/Bot%20robin%20menu.jpg",
+          },
+          caption: madeMenu,
+        },
+        { quoted: mek }
+      );
+    } catch (e) {
+      console.log(e);
+      reply(`${e}`);
+    }
+  }
+);
