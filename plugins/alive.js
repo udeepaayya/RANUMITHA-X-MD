72df63c5df1fd415f2bfdf9bbd3ae5c1eb258ab0
Ranumithaofc <hirukaranumithagitbota@gmail.com>
2025-07-24 18:49:15 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -3,7 +3,7 @@ const {cmd , commands} = require('../command')
 
 cmd({
     pattern: "alive",
-    alias: ["list","ranu"],
+    alias: ["hyranu","ranu"],
     desc: "Check bot online or no.",
     category: "main",
     filename: __filename
