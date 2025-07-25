0cfee1f1235234e6b76e037ea34b9eac2b75fa1b
Ranumithaofc <hirukaranumithagitbota@gmail.com>
2025-07-25 21:53:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "Enter your session ID",
+  SESSION_ID: process.env.SESSION_ID || "M3FGVJBJ#vQOoJTGPRMRxbz5JMdiDKqcD9rAuO_h1SAhmXiWuHFE",
   MONGODB: process.env.MONGODB || "mongodb://mongo:wOWITGyKuGkFUYAVcdpUmJGtbzewRZpZ@yamabiko.proxy.rlwy.net:36560",
   OWNER_NUM: process.env.OWNER_NUM || "94762095304",
 };
