f79d290330c5942127c32045127e285bc659091d
Ranumithaofc <hirukaranumithagitbota@gmail.com>
2025-07-26 23:11:29 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "M3FGVJBJ#vQOoJTGPRMRxbz5JMdiDKqcD9rAuO_h1SAhmXiWuHFE",
+  SESSION_ID: process.env.SESSION_ID || "l3dhnSbK#S5ppfxiKNhwrd82cpLtXSINjsuFU5T05fZUUPAkyNnU",
   MONGODB: process.env.MONGODB || "mongodb://mongo:OYakkuVAafWwqyhTSqNndhTOGdYmjPlS@switchback.proxy.rlwy.net:53940",
   OWNER_NUM: process.env.OWNER_NUM || "94762095304",
 };
