0597f538ce3484114763a707a899e4171da9d935
Ranumithaofc <hirukaranumithagitbota@gmail.com>
2025-07-31 21:10:42 +0530

    Update sticker2img.js

--- plugins/sticker2img.js
+++ plugins/sticker2img.js
@@ -43,18 +43,18 @@ cmd(
     try {
       // Ensure the message contains a sticker to convert
       if (!quoted || quoted.stickerMessage == null) {
-        return reply("Please reply to a sticker to convert it to an image.");
+        return reply("*Please reply to a sticker to convert it to an image.*");
       }
 
       // Download the sticker from the quoted message
       const stickerBuffer = await downloadMediaMessage(quoted, "stickerInput");
       if (!stickerBuffer)
-        return reply("Failed to download the sticker. Try again!");
+        return reply("*Failed to download the sticker. Try again!*");
 
       // Convert the sticker buffer to an image (using Sticker class)
       const sticker = new Sticker(stickerBuffer, {
-        pack: "𝐑_𝐎_𝐁_𝐈_𝐍",
-        author: "𝐒_𝐈_𝐇_𝐈_𝐋_𝐄_𝐋",
+        pack: "*𝗥𝗔𝗡𝗨𝗠𝗜𝗧𝗛𝗔-𝗫-𝗠𝗗*",
+        author: "OWN BY 𝗥𝗔𝗡𝗨𝗠𝗜𝗧𝗛𝗔-𝗫-𝗠𝗗",
         type: "FULL", // This may not be needed, but ensures we're using the full sticker format
         quality: 100, // Quality of the output image (0-100)
       });
@@ -67,7 +67,7 @@ cmd(
         from,
         {
           image: imageBuffer,
-          caption: "Here is your converted image!\n\n𝐌𝐚𝐝𝐞 𝐛𝐲 𝐒_𝐈_𝐇_𝐈_𝐋_𝐄_𝐋",
+          caption: "*Here is your converted image!*\n\nOWN BY 𝗥𝗔𝗡𝗨𝗠𝗜𝗧𝗛𝗔-𝗫-𝗠𝗗",
         },
         { quoted: mek }
       );
