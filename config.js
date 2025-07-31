bd225e444026d715d111d3553445076de42895df
Ranumithaofc <hirukaranumithagitbota@gmail.com>
2025-07-31 13:03:52 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ module.exports = {
   SESSION_ID: process.env.SESSION_ID || "l3dhnSbK#S5ppfxiKNhwrd82cpLtXSINjsuFU5T05fZUUPAkyNnU",
   MONGODB: process.env.MONGODB || "mongodb://mongo:zeeNflUdMhsBAvsFyxNLhUjZPjhuHMko@caboose.proxy.rlwy.net:46999",
   OWNER_NUM: process.env.OWNER_NUM || "94762095304",
-  PREFIX: process.env.PREFIX || ".",
+  PREFIX: process.env.PREFIX || "'",
   AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false",
   MODE: process.env.MODE || "public",
 };
