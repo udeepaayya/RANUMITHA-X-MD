eb6fb9d0bed84e7526b602f4b2e9d547cef845b8
Ranumithaofc <hirukaranumithagitbota@gmail.com>
2025-07-31 18:06:14 +0530

    Add files via upload

---
+++ plugins/fb.js
@@ -0,0 +1,64 @@
+const axios = require("axios");
+const cheerio = require('cheerio');
+const { cmd, commands } = require('../command')
+const config = require('../config');
+const {fetchJson} = require('../lib/functions');
+
+const api = `https://nethu-api-ashy.vercel.app`;
+
+cmd({
+  pattern: "facebook",
+  react: "🎥",
+  alias: ["fbb", "fbvideo", "fb"],
+  desc: "Download videos from Facebook",
+  category: "download",
+  use: '.facebook <facebook_url>',
+  filename: __filename
+},
+async(conn, mek, m, {
+    from, prefix, q, reply
+}) => {
+  try {
+  if (!q) return reply("*🚩 Please give me a facebook url🐼*");
+
+  const fb = await fetchJson(`${api}/download/fbdown?url=${encodeURIComponent(q)}`);
+  
+  if (!fb.result || (!fb.result.sd && !fb.result.hd)) {
+    return reply("I couldn't find anything :(");
+  }
+
+  let caption = `𝗥𝗔𝗡𝗨𝗠𝗜𝗧𝗛𝗔-𝗫-𝗠𝗗
+
+📝 ᴛɪᴛʟᴇ : Facebook video
+🦸‍♀️ ᴘᴏᴡᴇʀᴇᴅ ʙʏ : 𝗥𝗔𝗡𝗨𝗠𝗜𝗧𝗛𝗔-𝗫-𝗠𝗗
+🔗 ᴜʀʟ : ${q}`;
+
+
+  if (fb.result.thumb) {
+    await conn.sendMessage(from, {
+      image: { url: fb.result.thumb },
+      caption : caption,
+      }, mek);
+  }
+
+    if (fb.result.sd) {
+      await conn.sendMessage(from, {
+        video: { url: fb.result.sd },
+        mimetype: "video/mp4",
+        caption: `*SD-Quality*`
+      }, { quoted: mek });
+    }
+
+if (fb.result.hd) {
+      await conn.sendMessage(from, {
+        video: { url: fb.result.hd },
+        mimetype: "video/mp4",
+        caption: `*HD-Quality*`
+      }, { quoted: mek });
+    }
+
+} catch (err) {
+  console.error(err);
+  reply("*Failed to download video💔 Please try again later...* 🐼");
+  }
+});
