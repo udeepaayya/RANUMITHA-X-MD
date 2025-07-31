642ded6b5f5f8ff4590de5618ef68979165aec11
Ranumithaofc <hirukaranumithagitbota@gmail.com>
2025-07-31 20:58:22 +0530

    Update config.js

--- config.js
+++ config.js
@@ -13,4 +13,7 @@ module.exports = {
   ALIVE_MSG: process.env.ALIVE_MSG || "*👋 Hello, I am alive now !!*\n\n> 𝐌𝐚𝐝𝐞 𝐛𝐲 𝗥𝗔𝗡𝗨𝗠𝗜𝗧𝗛𝗔 🥶",
   AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
   MODE: process.env.MODE || "public",
+  AUTO_VOICE: process.env.AUTO_VOICE || "true",
+  AUTO_STICKER: process.env.AUTO_STICKER || "true",
+  AUTO_REPLY: process.env.AUTO_REPLY || "true",
 };
