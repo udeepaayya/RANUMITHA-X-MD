b10a57d4618370f95fb7613f70d9d0ae62c7c61e
Ranumithaofc <hirukaranumithagitbota@gmail.com>
2025-08-01 22:51:23 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -13,7 +13,7 @@ async(robin, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sende
 try{
     await robin.sendPresenceUpdate('recording', from);
     await robin.sendMessage(from, { audio: { url: "https://github.com/Ranumithaofc/RANU-FILE-S-/raw/refs/heads/main/Audio/Amor%20Na%20Praia%20(Slowed)%20edited.mp3" }, mimetype: 'audio/mpeg', ptt: true }, { quoted: mek });
-return await robin.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG},{quoted: mek})
+return await robin.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG,Hellow world for intarnational},{quoted: mek})
     
 }catch(e){
 console.log(e)
