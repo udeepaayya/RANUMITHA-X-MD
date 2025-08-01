442dce562a18cb72079cac9b084dd7932de2309e
Ranumithaofc <hirukaranumithagitbota@gmail.com>
2025-08-01 22:20:51 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -13,22 +13,10 @@ async(robin, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sende
 try{
     await robin.sendPresenceUpdate('recording', from);
     await robin.sendMessage(from, { audio: { url: "https://github.com/Ranumithaofc/RANU-FILE-S-/raw/refs/heads/main/Audio/Amor%20Na%20Praia%20(Slowed)%20edited.mp3" }, mimetype: 'audio/mpeg', ptt: true }, { quoted: mek });
-return await robin.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: const status = `
-╭───〔 *🤖 ${config.BOT_NAME} STATUS* 〕───◉
-│✨ *Bot is Active & Online!*
-│
-│👑 *Owner:* ${config.OWNER_NAME}
-│⚡ *Version:* 2.0.0
-│📝 *Prefix:* [${config.PREFIX}]
-│📳 *Mode:* [${config.MODE}]
-│💾 *RAM:* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${(os.totalmem() / 1024 / 1024).toFixed(2)}MB
-│🖥️ *Host:* ${os.hostname()}
-│⌛ *Uptime:* ${runtime(process.uptime())}
-╰────────────────────◉ config.ALIVE_MSG},{quoted: mek})
+return await robin.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG},{quoted: mek})
     
 }catch(e){
 console.log(e)
 reply(`${e}`)
 }
 })
-
