96dcf75461974aced17c57f60399d6d349f95b07
Ranumithaofc <hirukaranumithagitbota@gmail.com>
2025-08-01 20:21:08 +0530

    Update config.js

--- config.js
+++ config.js
@@ -13,17 +13,6 @@ module.exports = {
   MODE: process.env.MODE || "public",
   AUTO_VOICE: process.env.AUTO_VOICE || "true",
   AUTO_STICKER: process.env.AUTO_STICKER || "true",
-  AUTO_REPLY: process.env.AUTO_REPLY || "true",
-  ALIVE_IMG: process.env.ALIVE_IMG || "https://raw.githubusercontent.com/Ranumithaofc/RANU-FILE-S-/refs/heads/main/images/GridArt_20250726_193256660.jpg",
-  ALIVE_MSG: process.env.ALIVE_MSG || "*👋 Hello ${pushname}, I am alive now !!*
+  AUTO_REPLY: process.env.AUTO_REPLY || "true"
 
-*╭─「 ALIVE STATUS 」*
-*│* 🐼 *Bot*: *𝐑𝐀𝐍𝐔𝐌𝐈𝐓𝐇𝐀-𝐗-𝐌𝐃*
-*│* 👤 *User*: ${pushname}
-*│* 🤵‍♂ Owner: *ᴴᴵᴿᵁᴷᴬ ᴿᴬᴺᵁᴹᴵᵀᴴᴬ*
-*│* ⏰ Uptime: ${runtime(process.uptime())}
-*│* ⏳ Ram: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${(os.totalmem() / 1024 / 1024).toFixed(2)}MB
-*│* 🖊️ Prefix: ${config.PREFIX}
-╰──────────●●► 
-\n> 𝐌𝐚𝐝𝐞 𝐛𝐲 𝗥𝗔𝗡𝗨𝗠𝗜𝗧𝗛𝗔 🥶"
 };
