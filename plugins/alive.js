2e1f28974060f7f8ccc1dfa535c6405c0e251aaa
Ranumithaofc <hirukaranumithagitbota@gmail.com>
2025-08-02 10:56:28 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -43,10 +43,10 @@ async (robin, mek, m, {
         // Send Image + Caption
         await robin.sendMessage(from, {
             image: {
-                url: "https://raw.githubusercontent.com/Ranumithaofc/RANU-FILE-S-/refs/heads/main/images/GridArt_20250726_193256660.jpg" // You can replace this with your own ALIVE_IMG URL
-        };
+                url: "https://telegra.ph/file/32f84a39c9cd47a2e35f1.jpg" // You can replace this with your own ALIVE_IMG URL
+            }, { quoted: mek });
 
-        } catch (e) {
+    } catch (e) {
         console.log("Alive Error:", e);
         reply(`⚠️ Error: ${e.message}`);
     }
