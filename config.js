38f0bd17d44ebce0afe81578715305734be70501
Ranumithaofc <hirukaranumithagitbota@gmail.com>
2025-08-04 00:38:52 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,14 +6,22 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "Mv0T3RKI#ZNLA4TbGMxGucWaOvYE9wdmC66ElHm2vKgoKh8U2z54",
+  SESSION_ID: process.env.SESSION_ID || "Mv0T3RKI#ZNLA4TbGMxGucWaOvYE9wdmC66ElHm2vKgoKh8U2z54",   // add your Session Id 
   OWNER_NUM: process.env.OWNER_NUM || "94762095304",
   PREFIX: process.env.PREFIX || ".",
   AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-  MODE: process.env.MODE || "public",
+  MODE: process.env.MODE || "public",   // make bot public-private-inbox-group 
   AUTO_VOICE: process.env.AUTO_VOICE || "true",
   AUTO_STICKER: process.env.AUTO_STICKER || "true",
   AUTO_REPLY: process.env.AUTO_REPLY || "true",
+  ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true",
+  AUTO_RECORDING: process.env.AUTO_RECORDING || "false",
+  AUTO_TYPING: process.env.AUTO_TYPING || "false",
+  READ_MESSAGE: process.env.READ_MESSAGE || "false",
+  AUTO_REACT: process.env.AUTO_REACT || "false",
+  STICKER_NAME: process.env.STICKER_NAME || "RANUMITHA-X-MD",
+  BOT_NAME: process.env.BOT_NAME || "RANUMITHA-X-MD",
+  DESCRIPTION: process.env.DESCRIPTION || "*© Powerd by 𝗥𝗔𝗡𝗨𝗠𝗜𝗧𝗛𝗔-𝗫-𝗠𝗗 🌛*",
   ALIVE_IMG: process.env.ALIVE_IMG || "https://raw.githubusercontent.com/Ranumithaofc/RANU-FILE-S-/refs/heads/main/images/GridArt_20250726_193256660.jpg",
   ALIVE_MSG: process.env.ALIVE_MSG || "> 𝐌𝐚𝐝𝐞 𝐛𝐲 𝗥𝗔𝗡𝗨𝗠𝗜𝗧𝗛𝗔 🥶"
 };
