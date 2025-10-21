a3eded1c3fa60be993179f8c2a9b33ed2848c53a
Ranumithaofc <hirukaranumithagitbota@gmail.com>
2025-10-21 17:29:59 +0530

    Update config.js

--- config.js
+++ config.js
@@ -20,7 +20,7 @@ MODE: process.env.MODE || "public",
 // make bot public-private-inbox-group
 PUBLIC_MODE: process.env.PUBLIC_MODE || "true",
 // make false if want private mod
-AUTO_VOICE: process.env.AUTO_VOICE || "false",
+AUTO_VOICE: process.env.AUTO_VOICE || "true",
 // make true for send automatic voices
 AUTO_STICKER: process.env.AUTO_STICKER || "true",
 // make true for automatic stickers 
