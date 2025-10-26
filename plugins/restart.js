423ff9aa691e3e1102b1d298cf34487530d0149b
udeepaayya <udeepaayyaofc@gmail.com>
2025-10-26 19:46:35 +0530

    Update restart.js

--- plugins/restart.js
+++ plugins/restart.js
@@ -1,29 +1,25 @@
-const { cmd } = require("../command");
-const { sleep } = require("../lib/functions");
+const { cmd } = require("../command");  
+const { sleep } = require("../lib/functions");  
 
-cmd({
-    pattern: "restart",
+cmd({  
+    pattern: "restart",  
     react: '🔄',
-    desc: "Restart RANUMITHA-X-MD",
-    category: "owner",
-    filename: __filename
-},
-async (conn, mek, m, { reply, isOwner }) => {
-    try {
-        if (!isOwner) {
-            await conn.sendMessage(m.chat, { react: { text: '🚫', key: mek.key } }); 
-            return reply("*🚫 Owner only command!*");
-        }
+    desc: "Restart RANUMITHA-X-MD",  
+    category: "owner",  
+    filename: __filename  
+},  
+async (conn, mek, m, { reply, isOwner }) => {  
+    try {  
+        if (!isOwner) {  
+            return reply("*🚫 Owner only command!*");  
+        }  
 
-        await conn.sendMessage(m.chat, { react: { text: '✔️', key: mek.key } }); 
-        reply("Restarting...");
-        await sleep(1500);
-
-        const { exec } = require("child_process");
-        exec("pm2 restart all");
-
-    } catch (e) {
-        console.error(e);
-        reply(`${e}`);
-    }
+        const { exec } = require("child_process");  
+        reply("Restarting...");  
+        await sleep(1500);  
+        exec("pm2 restart all");  
+    } catch (e) {  
+        console.error(e);  
+        reply(`${e}`);  
+    }  
 });
