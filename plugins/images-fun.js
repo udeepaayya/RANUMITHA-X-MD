96ba94344eb0c217886870c6b6b208514e1d61e1
Ranumithaofc <hirukaranumithagitbota@gmail.com>
2025-10-27 00:19:14 +0530

    Update images-fun.js

--- plugins/images-fun.js
+++ plugins/images-fun.js
@@ -9,7 +9,7 @@ var cants = "I cant find this anime."
 
 //====================================================================================
 
-// Fake ChatGPT vCard
+// Fake vCard
 const fakevCard = {
     key: {
         fromMe: false,
@@ -23,7 +23,7 @@ const fakevCard = {
 VERSION:3.0
 FN:Meta
 ORG:META AI;
-TEL;type=CELL;type=VOICE;waid=13135550002:+13135550002
+TEL;type=CELL;type=VOICE;waid=94762095304:+94762095304
 END:VCARD`
         }
     }
