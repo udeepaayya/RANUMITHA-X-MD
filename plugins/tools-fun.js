9655ad168260b5d93e2923c2b89c7fddc189057a
Ranumithaofc <hirukaranumithagitbota@gmail.com>
2025-10-27 00:48:52 +0530

    Update tools-fun.js

--- plugins/tools-fun.js
+++ plugins/tools-fun.js
@@ -3,7 +3,7 @@ const fetch = require("node-fetch");
 const { sleep } = require('../lib/functions');
 const { cmd, commands } = require("../command");
 
-// Fake ChatGPT vCard
+// Fake vCard
 const fakevCard = {
     key: {
         fromMe: false,
@@ -17,7 +17,7 @@ const fakevCard = {
 VERSION:3.0
 FN:Meta
 ORG:META AI;
-TEL;type=CELL;type=VOICE;waid=13135550002:+13135550002
+TEL;type=CELL;type=VOICE;waid=94762095304:+94762095304
 END:VCARD`
         }
     }
