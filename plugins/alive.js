0a356bff325c2cdf0d13a2253c03d115b20185ca
Ranumithaofc <hirukaranumithagitbota@gmail.com>
2025-10-30 11:04:19 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -40,7 +40,7 @@ async (robin, mek, m, {
         // Voice Note
         await robin.sendMessage(from, {
             audio: {
-                url: "https://github.com/Ranumithaofc/RANU-FILE-S-/raw/refs/heads/main/Audio/Alive%20new%20typ.opus"
+                url: "https://github.com/Ranumithaofc/RANU-FILE-S-/raw/refs/heads/main/Audio/Ranumitha-x-md-Alive-org.opus"
             },
             mimetype: 'audio/mp4',
             ptt: true
