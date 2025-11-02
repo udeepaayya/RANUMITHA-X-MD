8c4467e7ea66bb0aa2de1097b6ef41b42d6112d1
Ranumithaofc <hirukaranumithagitbota@gmail.com>
2025-11-02 17:26:17 +0530

    Create pair.js

---
+++ plugins/pair.js
@@ -0,0 +1,42 @@
+const { cmd } = require('../command');
+const axios = require('axios');
+
+
+cmd({
+    pattern: "pair",
+    alias: ["getpair", "clonebot"],
+    react: "✅",
+    desc: "Get pairing code for WHITESHADOW-MD bot",
+    category: "download",
+    use: ".pair +947XXXXXXXX",
+    filename: __filename
+}, async (conn, mek, m, { q, senderNumber, reply }) => {
+    try {
+        const phoneNumber = q ? q.trim() : senderNumber;
+
+        if (!phoneNumber || !phoneNumber.match(/^\+?\d{10,15}$/)) {
+            return await reply("❌ Please provide a valid phone number with country code\nExample: .pair +94XXXXXXXXX");
+        }
+
+        // Remove + sign for API
+        const cleanNumber = phoneNumber.replace(/\D/g, "");
+
+        // Call API endpoint
+        const res = await axios.get(`https://whiteshadow-8182be1f6ed6.herokuapp.com/code?number=${cleanNumber}`);
+        const code = res.data?.code;
+
+        if (!code) {
+            return await reply("❌ Could not retrieve BILAL-MD pairing code.");
+        }
+
+        const doneMessage = "> *WHITESHADOW-MD PAIRING COMPLETED ☑️*";
+        await reply(`${doneMessage}\n\n*Your pairing code is:* ${code}\n\n👾 \n`Please edit the Season id like this :-\n` SESSION ID එකේ මුලට *White-MD~* වෙනුවට මුලට *ranu&* විදිහට Edit කරගන්න.`);
+
+        await new Promise(resolve => setTimeout(resolve, 2000));
+        await reply(`${code}`);
+
+    } catch (err) {
+        console.error("Pair1 command error:", err);
+        await reply("❌ Error while getting WHITESHADOW-MD pairing code.");
+    }
+});
