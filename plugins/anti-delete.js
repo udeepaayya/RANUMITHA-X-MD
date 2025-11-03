0dc2a797377cfe363ee6ceba68841f618fc3b742
Ranumithaofc <hirukaranumithagitbota@gmail.com>
2025-11-03 14:05:25 +0530

    Update anti-delete.js

--- plugins/anti-delete.js
+++ plugins/anti-delete.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
 const path = require('path');
-const config = require('../config')
-const {cmd , commands} = require('../command')
+const { getAnti, setAnti } = require('../data/antidel');
+const config = require('../config');
+const {cmd , commands} = require('../command');
 
 // Auto enable if config says true
 (async () => {
