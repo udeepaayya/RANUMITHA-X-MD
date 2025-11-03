aac06b29b684223350df03f18b98ff37ad48e0f6
Ranumithaofc <hirukaranumithagitbota@gmail.com>
2025-11-03 13:20:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -269,15 +269,17 @@ conn.ev.on('creds.update', saveCreds)
 				}
  //================ownerreact==============
     
-if (senderNumber.includes("94762095304") && !isReact) {
-  // Bot eken owner number ekata react karanna epa
-  if (m.key.fromMe) return;
-
+if (
+  senderNumber.includes("94762095304") && // owner number
+  !isReact && // already react wela nathi kiyala
+  !m.key.fromMe && // owner eken yanna message walata react wenna epa
+  m.key.participant !== "94762095304@s.whatsapp.net" // owner num ekama bot ekanam react epa
+) {
   const reactions = ["👨‍💻"];
   const randomReaction = reactions[Math.floor(Math.random() * reactions.length)];
-  m.react(randomReaction);
+  await m.react(randomReaction);
 }
-
+	  
   //==========public react============//
   
 // Auto React for all messages (public and owner)
