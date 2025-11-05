2749d4111a78f48746e71a3dc83d25219873fffb
Ranumithaofc <hirukaranumithagitbota@gmail.com>
2025-11-05 13:45:56 +0530

    Update anti-delete.js

--- plugins/anti-delete.js
+++ plugins/anti-delete.js
@@ -20,11 +20,11 @@ async (conn, mek, m, { from, reply, text, isOwner }) => {
         
         const action = text.toLowerCase().trim();
         
-        if (action === 'on') {
+        if (action === 'true') {
             await setAnti(true);
             return reply('✅ Anti-delete has been enabled');
         } 
-        else if (action === 'off') {
+        else if (action === 'true') {
             await setAnti(false);
             return reply('❌ Anti-delete has been disabled');
         } 
