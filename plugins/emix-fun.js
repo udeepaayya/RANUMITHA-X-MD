5db037611fc5653a7d5fa5fa4ff9a745c7fa8a3a
Ranumithaofc <hirukaranumithagitbota@gmail.com>
2025-11-15 18:10:44 +0530

    Update emix-fun.js

--- plugins/emix-fun.js
+++ plugins/emix-fun.js
@@ -31,7 +31,7 @@ cmd({
         let buffer = await getBuffer(imageUrl);
         let sticker = new Sticker(buffer, {
             pack: "Emoji Mix",
-            author: "WHITESHADOW-MD",
+            author: "RANUMITHA-X-MD",
             type: StickerTypes.FULL,
             categories: ["🤩", "🎉"],
             quality: 75,
