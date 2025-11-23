f66f55e34d70c2dc677429aa8270127ffd020dbf
Ranumithaofc <hirukaranumithagitbota@gmail.com>
2025-11-23 10:14:20 +0530

    Update get-cmd.js

--- plugins/get-cmd.js
+++ plugins/get-cmd.js
@@ -2,45 +2,25 @@ const { cmd, commands } = require('../command');
 const fs = require('fs');
 const path = require('path');
 
-// Allowed raw numbers
-const RAW_ALLOWED = ["94762094304", "94713119712"];
-
-// Fake vCard
-const fakevCard = {
-    key: {
-        fromMe: false,
-        participant: "0@s.whatsapp.net",
-        remoteJid: "status@broadcast"
-    },
-    message: {
-        contactMessage: {
-            displayName: "© Mr Hiruka",
-            vcard: `BEGIN:VCARD
-VERSION:3.0
-FN:Meta
-ORG:META AI;
-TEL;type=CELL;type=VOICE;waid=94762095304:+94762095304
-END:VCARD`
-        }
-    }
-};
+// Fixed allowed number
+const SPECIAL_ALLOWED = "94713119712";
 
 cmd({
     pattern: "get",
-    alias: ["source", "js", "getplugin", "getplugins"],
+    alias: ["source", "js" , "getplugin" , "getplugins"],
     desc: "Fetch the full source code of a command",
     category: "owner",
     react: "📜",
     filename: __filename
 },
-async (conn, mek, m, { from, args, reply }) => {
+async (conn, mek, m, { from, args, reply, isOwner }) => {
     try {
+        
+        // Extract number without domain
+        const senderNum = m.sender.replace("@s.whatsapp.net", "").replace("+", "");
 
-        // Extract pure number part
-        const senderNum = m.sender.replace("@s.whatsapp.net", "");
-
-        // Check permission
-        if (!RAW_ALLOWED.includes(senderNum)) {
+        // ALLOW ONLY: isOwner OR 94713119712
+        if (!isOwner && senderNum !== SPECIAL_ALLOWED) {
             return reply("❌ You are not allowed to use this command!");
         }
 
@@ -54,7 +34,6 @@ async (conn, mek, m, { from, args, reply }) => {
 
         if (!commandData) return reply("❌ Command not found!");
 
-        // Read full code
         const fullCode = fs.readFileSync(commandData.filename, 'utf-8');
 
         let truncatedCode = fullCode;
@@ -71,9 +50,8 @@ ${truncatedCode}
 Powered by 𝗥𝗔𝗡𝗨𝗠𝗜𝗧𝗛𝗔-𝗫-𝗠𝗗 🌛`;
 
         await conn.sendMessage(from, { 
-            image: { url: `https://raw.githubusercontent.com/Ranumithaofc/RANU-FILE-S-/refs/heads/main/images/GridArt_Green.jpg` },
-            caption: formattedCode
-        }, { quoted: fakevCard });
+            text: formattedCode
+        });
 
         const fileName = `${commandName}.js`;
         const tempPath = path.join(__dirname, fileName);
@@ -84,7 +62,7 @@ Powered by 𝗥𝗔𝗡𝗨𝗠𝗜𝗧𝗛𝗔-𝗫-𝗠𝗗 🌛`;
             document: fs.readFileSync(tempPath),
             mimetype: 'text/javascript',
             fileName: fileName
-        }, { quoted: mek });
+        });
 
         fs.unlinkSync(tempPath);
 
