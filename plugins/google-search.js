0724b42a460d9a5c5220205ecf896fab4ac6d5fa
Ranumithaofc <hirukaranumithagitbota@gmail.com>
2025-11-26 20:21:31 +0530

    Update google-search.js

--- plugins/google-search.js
+++ plugins/google-search.js
@@ -1,29 +1,10 @@
 const { cmd } = require('../command');
 const axios = require('axios');
 
-// Fake ChatGPT vCard
-const fakevCard = {
-    key: {
-        fromMe: false,
-        participant: "0@s.whatsapp.net",
-        remoteJid: "status@broadcast"
-    },
-    message: {
-        contactMessage: {
-            displayName: "© Mr Hiruka",
-            vcard: `BEGIN:VCARD
-VERSION:3.0
-FN:Meta
-ORG:META AI;
-TEL;type=CELL;type=VOICE;waid=94762095304:+94762095304
-END:VCARD`
-        }
-    }
-};
 
 cmd({
   pattern: "google",
-  alias: ["gsearch","gsh"],
+  alias: ["gsearch"],
   desc: "Search Google via Chamod API",
   category: "search",
   react: "🔎",
@@ -31,13 +12,9 @@ cmd({
 },
 async (conn, mek, m, { text }) => {
   try {
-
     if (!text) return m.reply("Please provide a search query!\nExample: .google tiktok");
 
-    // 💬 First send the fake vCard
-    await conn.sendMessage(m.chat, fakevCard, { quoted: mek });
-
-    // API call
+    // Cloudflare Worker API call
     const apiUrl = `https://google-search-api.chamodshadow125.workers.dev/?q=${encodeURIComponent(text)}`;
     const res = await axios.get(apiUrl);
 
@@ -46,14 +23,10 @@ async (conn, mek, m, { text }) => {
       res.data.data.slice(0, 5).forEach((item, i) => {
         msg += `*${i+1}.* ${item.title}\n${item.link}\n_${item.snippet}_\n\n`;
       });
-
-      // Send results quoted using the fake vCard
-      conn.sendMessage(m.chat, { text: msg }, { quoted: fakevCard });
-
+      m.reply(msg);
     } else {
       m.reply("No results found 😔");
     }
-
   } catch (error) {
     console.error(error);
     m.reply("Error fetching results.");
