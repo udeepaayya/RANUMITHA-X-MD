ad2c0dee7fa6b1a015793e8987c5f955c2bcb4fa
Ranumithaofc <hirukaranumithagitbota@gmail.com>
2025-12-07 12:48:14 +0530

    Update fancy-text.js

--- plugins/fancy-text.js
+++ plugins/fancy-text.js
@@ -15,15 +15,16 @@ const fakevCard = {
 VERSION:3.0
 FN:Meta
 ORG:META AI;
-TEL;type=CELL;type=VOICE;waid=13135550002:+13135550002
+TEL;type=CELL;type=VOICE;waid=94762095304:+94762095304
 END:VCARD`
         }
     }
 };
 
+
 cmd({
   pattern: "fancy",
-  alias: ["font", "style","ranufont","ranufonts"],
+  alias: ["font", "style"],
   react: "✍️",
   desc: "Convert text into various fonts.",
   category: "tools",
@@ -31,20 +32,20 @@ cmd({
 }, async (conn, m, store, { from, quoted, args, q, reply }) => {
   try {
     if (!q) {
-      return reply("❎ Please provide text to convert into fancy fonts.\n\n*Example:* .fancy *text/name*");
+      return reply("❎ Please provide text to convert into fancy fonts.\n\n*Example:* .fancy Hello");
     }
 
-    const apiUrl = `https://www.dark-yasiya-api.site/other/font?text=${encodeURIComponent(q)}`;
+    const apiUrl = `https://api.giftedtech.co.ke/api/tools/fancy?apikey=gifted&text=${encodeURIComponent(q)}`;
     const response = await axios.get(apiUrl);
     
     if (!response.data.status) {
       return reply("❌ Error fetching fonts. Please try again later.");
     }
 
-    const fonts = response.data.result.map(item => `*${item.name}:*\n${item.result}`).join("\n\n");
+    const fonts = response.data.results.map(item => `*${item.name}:*\n${item.result}`).join("\n\n");
     const resultText = `✨ *Fancy Fonts Converter* ✨\n\n${fonts}\n\n> © Powerd by 𝗥𝗔𝗡𝗨𝗠𝗜𝗧𝗛𝗔-𝗫-𝗠𝗗 🌛`;
 
-    await conn.sendMessage(from, { text: resultText }, { quoted: fakevCard }, { quoted: m });
+    await conn.sendMessage(from, { text: resultText }, { quoted: fakevCard });
   } catch (error) {
     console.error("❌ Error in fancy command:", error);
     reply("⚠️ An error occurred while fetching fonts.");
